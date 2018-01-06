3cd7e1eced3d7e833d075ec97f13d9d74ab81206
stevesdm <stevejobson@163.com>
2018-01-06 17:30:05 +0800

    init the project

---
+++ src/main/java/com/steve/microsharding/MicroShardingApplication.java
@@ -0,0 +1,12 @@
+package com.steve.microsharding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroShardingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroShardingApplication.class, args);
+	}
+}
